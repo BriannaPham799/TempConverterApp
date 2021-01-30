fcd42647b689e663cdf5e4ae996aba8ee06ae6f1
Uyen[Brianna] Pham <brianna.pham799@gmail.com>
2021-01-30 10:58:24 -0800

    new page was created

---
+++ ConversionApp/app/src/main/java/com/example/tempconverseapp/cTof.java
@@ -0,0 +1,14 @@
+package com.example.tempconverseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cTof extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_tof);
+    }
+}
